f96b3efe83af02164849b57d462c7a11ebfec6e6
Steven Yung <stevenyung@fastmail.com>
2021-07-12 16:42:52 +0200

    fix search repo form issue & finish design for new blocks

--- apps/web/src/views/Add.tsx
+++ apps/web/src/views/Add.tsx
@@ -121,7 +121,7 @@ const CreateBlock: Component = () => {
 
 export default CreateBlock;
 
-const NewBlockModal: Component<{ block: Block; handleClose: () => void }> = (
+const NewBlockModal: Component<{ block: Block; closeModal: () => void }> = (
   props
 ) => {
   props = mergeProps({ block: blocks()[1] }, props);
@@ -142,7 +142,7 @@ const NewBlockModal: Component<{ block: Block; handleClose: () => void }> = (
           </div>
 
           <ChooseBlockTypeForm
-            onCancel={props.handleClose}
+            onCancel={props.closeModal}
             onSubmit={() => setStepIndex((stepIndex) => stepIndex + 1)}
           />
         </Match>
@@ -170,7 +170,10 @@ const NewBlockModal: Component<{ block: Block; handleClose: () => void }> = (
               </div>
             )}
           >
-            <SearchRepoForm />
+            <SearchRepoForm
+              onSubmit={props.closeModal}
+              onCancel={props.closeModal}
+            />
           </ErrorBoundary>
         </Match>
       </Switch>
@@ -254,7 +257,10 @@ const ChooseBlockTypeForm: Component<{
   );
 };
 
-const SearchRepoForm: Component = () => {
+const SearchRepoForm: Component<{
+  onSubmit: () => void;
+  onCancel: () => void;
+}> = (props) => {
   type Repo = { id: number; full_name: string };
 
   const [formData, setFormData] = createStore<{
@@ -287,8 +293,14 @@ const SearchRepoForm: Component = () => {
   const [selectedRepo, setSelectedRepo] = createSignal<Repo>(null);
 
   return (
-    <>
-      <div class="my-6">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.onSubmit();
+      }}
+      class="flex flex-col gap-6 mt-6"
+    >
+      <div>
         <SearchField
           model={[formData, setFormData]}
           placeholder="Search Repository"
@@ -302,7 +314,39 @@ const SearchRepoForm: Component = () => {
         style="min-height: 120px;"
       >
         <Show when={!repositories()}>
-          <span class="font-mono">Find your repository by name</span> ☝️
+          <div class="flex gap-2 items-baseline">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3 w-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 7l4-4m0 0l4 4m-4-4v18"
+              />
+            </svg>
+
+            <span class="font-mono">Find your repository by name</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3 w-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 7l4-4m0 0l4 4m-4-4v18"
+              />
+            </svg>
+          </div>
         </Show>
 
         <Show when={selectedRepo()}>
@@ -311,7 +355,7 @@ const SearchRepoForm: Component = () => {
               <button
                 onClick={() => setSelectedRepo(null)}
                 type="button"
-                class="cursor-pointer flex gap-3 items-center"
+                class="cursor-pointer flex gap-3 items-center w-full"
               >
                 <span class="w-5 h-5 rounded-md bg-red-700 text-white flex justify-center items-center">
                   <Minus class="w-3 h-3" />
@@ -332,7 +376,7 @@ const SearchRepoForm: Component = () => {
                     <button
                       onClick={() => setSelectedRepo(repository)}
                       type="button"
-                      class="cursor-pointer flex gap-3 items-center"
+                      class="cursor-pointer flex gap-3 items-center w-full"
                     >
                       <span class="w-5 h-5 rounded-md bg-green-700 text-white flex justify-center items-center">
                         <Plus class="w-3 h-3" />
@@ -367,7 +411,7 @@ const SearchRepoForm: Component = () => {
           Cancel
         </button>
       </div>
-    </>
+    </form>
   );
 };
 
