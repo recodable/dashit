91e18a43c207f632705c7a9c9c62c83c240af220
Steven Yung <stevenyung@fastmail.com>
2021-07-17 18:00:08 +0200

    (web) implement authentication design

--- apps/web/src/registry.ts
+++ apps/web/src/registry.ts
@@ -1,51 +1,55 @@
 import { lazy } from "solid-js";
 import type { RegisteredBlock } from "./types";
 
-const registry: RegisteredBlock[] = [
-  {
-    name: "Github Star",
-    description:
-      "See all your Github stars evolution over time on one repository",
-    Component: lazy(() => import("./github/StarBlock")),
-    type: "github/StarBlock",
-    setup: {
-      name: "Select repository",
-      description: "We will track the star from this repository",
-      Component: lazy(() => import("./github/SearchRepoForm")),
+const registry: { [k: string]: RegisteredBlock[] } = {
+  github: [
+    {
+      name: "Github Star",
+      description:
+        "See all your Github stars evolution over time on one repository",
+      Component: lazy(() => import("./github/StarBlock")),
+      type: "github/StarBlock",
+      setup: {
+        name: "Select repository",
+        description: "We will track the star from this repository",
+        Component: lazy(() => import("./github/SearchRepoForm")),
+      },
+      trendable: true,
     },
-    trendable: true,
-  },
-  {
-    name: "Github Issue",
-    description:
-      "See how  many issues are open at the moment on one repository",
-    Component: lazy(() => import("./github/OpenIssueBlock")),
-    type: "github/OpenIssueBlock",
-    setup: {
-      name: "Select repository",
-      description: "We will track the open issue count from this repository",
-      Component: lazy(() => import("./github/SearchRepoForm")),
+    {
+      name: "Github Issue",
+      description:
+        "See how  many issues are open at the moment on one repository",
+      Component: lazy(() => import("./github/OpenIssueBlock")),
+      type: "github/OpenIssueBlock",
+      setup: {
+        name: "Select repository",
+        description: "We will track the open issue count from this repository",
+        Component: lazy(() => import("./github/SearchRepoForm")),
+      },
     },
-  },
-  {
-    name: "Github PR",
-    description: "See how  many PR are open at the moment on one reposirory",
-    Component: lazy(() => import("./github/OpenPullRequestBlock")),
-    type: "github/OpenPullRequestBlock",
-    setup: {
-      name: "Select repository",
-      description: "We will track the open PR count from this repository",
-      Component: lazy(() => import("./github/SearchRepoForm")),
+    {
+      name: "Github PR",
+      description: "See how  many PR are open at the moment on one reposirory",
+      Component: lazy(() => import("./github/OpenPullRequestBlock")),
+      type: "github/OpenPullRequestBlock",
+      setup: {
+        name: "Select repository",
+        description: "We will track the open PR count from this repository",
+        Component: lazy(() => import("./github/SearchRepoForm")),
+      },
     },
-  },
-  // {
-  //   name: "NPM Download",
-  //   description: "All NPM downloads informations",
-  //   Component: lazy(() => import("./npm/DownloadBlock")),
-  //   type: "npm/DownloadBlock",
-  //   // Setup: lazy(() => import("./github/SearchRepoForm")),
-  //   trendable: true,
-  // },
-];
+  ],
+  npm: [
+    {
+      name: "NPM Download",
+      description: "All NPM downloads informations",
+      Component: lazy(() => import("./npm/DownloadBlock")),
+      type: "npm/DownloadBlock",
+      // Setup: lazy(() => import("./github/SearchRepoForm")),
+      trendable: true,
+    },
+  ],
+};
 
 export default registry;
--- apps/web/src/views/Add.tsx
+++ apps/web/src/views/Add.tsx
@@ -2,18 +2,15 @@ import type { Component } from "solid-js";
 import { createSignal, mergeProps } from "solid-js";
 import { createStore } from "solid-js/store";
 import { For, Dynamic, Show, Index, ErrorBoundary } from "solid-js/web";
-import { TransitionGroup } from "solid-transition-group";
+import { Transition, TransitionGroup } from "solid-transition-group";
 import { setModal } from "@guillotin/solid";
 import SearchField from "../SearchField";
-// import debounce from "lodash.debounce";
 import { Link } from "solid-app-router";
 import { ChevronLeft, PlusCircle } from "../icons";
 import createHotkey from "../hotkey";
 import registry from "../registry";
 import type { RegisteredBlock } from "../types";
 import { useRouter } from "solid-app-router";
-
-const [blocks] = createSignal<RegisteredBlock[]>(registry);
 
 const needBlockSetup = (block: RegisteredBlock): boolean => !!block.setup;
 
@@ -43,62 +40,121 @@ const CreateBlock: Component = () => {
           </div>
         </div>
 
-        <ul>
-          <li class="p-6 rounded-lg bg-dark-gradiant">
-            <h2 class="mb-6">Github</h2>
+        <ul class="grid grid-cols-1 gap-y-14">
+          <For each={Object.entries(registry)}>
+            {([key, blocks]) => {
+              const searchedBlocks = () =>
+                blocks.filter(({ name }) => {
+                  return name
+                    .toLowerCase()
+                    .includes(formData.search.toLowerCase());
+                });
 
-            <ul class="grid grid-cols-3 gap-4">
-              <TransitionGroup
-                enterActiveClass="transition ease-in-out duration-100"
-                enterClass="opacity-0"
-                enterToClass="opacity-100"
-                exitActiveClass="transition ease-in-out duration-100"
-                exitClass="opacity-100"
-                exitToClass="opacity-0"
-              >
-                <For
-                  each={blocks().filter(({ name }) => {
-                    return name
-                      .toLowerCase()
-                      .includes(formData.search.toLowerCase());
-                  })}
+              const [hovered, setHovered] = createSignal(false);
+
+              return (
+                <li
+                  onMouseEnter={() => setHovered(true)}
+                  onMouseLeave={() => setHovered(false)}
+                  class="p-6 rounded-lg bg-dark-gradiant relative overflow-hidden"
                 >
-                  {(block) => {
-                    const [hovered, setHovered] = createSignal(false);
+                  <Transition
+                    enterActiveClass="z-10 transition-opacity ease-in-out duration-100"
+                    enterClass="opacity-0"
+                    enterToClass="opacity-100"
+                    exitActiveClass="transition-opacity ease-in-out duration-100"
+                    exitClass="opacity-100"
+                    exitToClass="opacity-0"
+                  >
+                    <Show when={hovered()}>
+                      <div class="absolute inset-0 flex flex-col gap-6 justify-center items-center">
+                        <div class="absolute inset-0 bg-gray-500 opacity-70 flex flex-col gap-6 justify-center items-center text-purple-200 z-20" />
 
-                    return (
-                      <li
-                        onClick={() => {
-                          if (!needBlockSetup(block)) {
-                            return push(dashboardUrl);
-                          }
+                        <div class="z-20 flex flex-col gap-6 justify-center items-center">
+                          <p class="text-xl font-semibold">
+                            You need to authenticate to{" "}
+                            <span class="capitalize">{key}</span>
+                          </p>
 
-                          setModal({
-                            Component: NewBlockModal,
-                            data: { block },
-                          });
+                          <button
+                            class="bg-black border-gray-900 px-7 py-4 shadow-lg text-lg font-semibold flex gap-3 items-center hover:bg-gray-900"
+                            type="button"
+                          >
+                            <span>
+                              Connect to <span class="capitalize">{key}</span>
+                            </span>
+
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-5 w-5"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </Show>
+                  </Transition>
+
+                  <h2 class="mb-6 capitalize">{key}</h2>
+
+                  <ul class="grid grid-cols-3 gap-4">
+                    <TransitionGroup
+                      enterActiveClass="transition-opacity ease-in-out duration-100"
+                      enterClass="opacity-0"
+                      enterToClass="opacity-100"
+                      exitActiveClass="transition-opacity ease-in-out duration-100"
+                      exitClass="opacity-100"
+                      exitToClass="opacity-0"
+                    >
+                      <For each={searchedBlocks()}>
+                        {(block) => {
+                          const [hovered, setHovered] = createSignal(false);
+
+                          return (
+                            <li
+                              onClick={() => {
+                                if (!needBlockSetup(block)) {
+                                  return push(dashboardUrl);
+                                }
+
+                                setModal({
+                                  Component: NewBlockModal,
+                                  data: { block },
+                                });
+                              }}
+                              onMouseEnter={() => setHovered(true)}
+                              onMouseLeave={() => setHovered(false)}
+                              class="small-card transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+                            >
+                              <Show when={hovered()}>
+                                <div class="absolute inset-0 bg-purple-600 opacity-70 flex justify-center items-center text-purple-200">
+                                  <PlusCircle class="w-7 h-7" />
+                                </div>
+                              </Show>
+
+                              <Dynamic<{ isPreview: boolean }>
+                                component={block.Component}
+                                isPreview
+                              />
+                            </li>
+                          );
                         }}
-                        onMouseEnter={() => setHovered(true)}
-                        onMouseLeave={() => setHovered(false)}
-                        class="small-card transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-                      >
-                        <Show when={hovered()}>
-                          <div class="absolute inset-0 bg-purple-600 opacity-70 flex justify-center items-center text-purple-200">
-                            <PlusCircle class="w-7 h-7" />
-                          </div>
-                        </Show>
-
-                        <Dynamic<{ isPreview: boolean }>
-                          component={block.Component}
-                          isPreview
-                        />
-                      </li>
-                    );
-                  }}
-                </For>
-              </TransitionGroup>
-            </ul>
-          </li>
+                      </For>
+                    </TransitionGroup>
+                  </ul>
+                </li>
+              );
+            }}
+          </For>
         </ul>
       </div>
     </div>
