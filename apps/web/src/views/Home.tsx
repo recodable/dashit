2e07e2bb9a92e66945c0a73d8482f2d696dfe48f
Steven Yung <stevenyung@fastmail.com>
2021-07-17 21:39:52 +0200

    (web) add redirection from home to dashboards if authenticated

--- apps/web/src/views/Home.tsx
+++ apps/web/src/views/Home.tsx
@@ -1,4 +1,16 @@
+import { createEffect } from "solid-js";
+import { useRouter } from "solid-app-router";
+import { useAuth0 } from "@rturnq/solid-auth0";
+
 const Home = () => {
+  const [, { replace }] = useRouter();
+  const { isAuthenticated } = useAuth0();
+
+  createEffect(() => {
+    if (!isAuthenticated()) return;
+    replace("/dashboards");
+  });
+
   return <h1>Home</h1>;
 };
 
