0674b896d7cf94dc910d65762068980f6a055a59
Steven Yung <stevenyung@fastmail.com>
2021-07-17 13:00:41 +0200

    (web) display auth0 user picture

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -246,7 +246,7 @@ const Navbar = () => {
 
 const ProfileDropdown = () => {
   const [isOpen, setIsOpen] = createSignal(false);
-  const { logout } = useAuth0();
+  const { user, logout } = useAuth0();
 
   return (
     <div class="ml-3 relative">
@@ -262,8 +262,8 @@ const ProfileDropdown = () => {
           <span class="sr-only">Open user menu</span>
           <img
             class="h-8 w-8 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
+            src={user()?.picture}
+            alt="Your profile picture"
           />
         </button>
       </div>
